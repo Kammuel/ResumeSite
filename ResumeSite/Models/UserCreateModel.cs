8308dfa835c8c130781ab848c74220f6b0318eac
Kammuel <kammfreudenstein@gmail.com>
2019-03-15 16:37:23 -0700

    UserCreateModel and User model added

---
+++ ResumeSite/Models/UserCreateModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ResumeSite.Models
+{
+	public class UserCreateModel
+	{
+		public string Username { get; set; }
+
+		[Required]
+		[StringLength(20, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+		[RegularExpression(@"^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)).+$")]
+		[DataType(DataType.Password)]
+		[Display(Name = "Password")]
+		public string Password { get; set; }
+
+		[Required]
+		[Compare("Password", ErrorMessage = "Passwords must match.")]
+		public string RepeatPassword { get; set; }
+
+		[EmailAddress]
+		public string EmailAddress { get; set; }
+
+
+
+	}
+}
